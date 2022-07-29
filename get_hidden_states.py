42b5ebadd2b977e9bf180ea4a431aedae413c5e3
Samuel Miller <55806453+samm00@users.noreply.github.com>
2022-07-29 16:05:16 -0400

    Update get_hidden_states.py

--- get_hidden_states.py
+++ get_hidden_states.py
@@ -7,6 +7,7 @@ import sys
 import numpy as np
 
 batches = 25
+lang_id = sys.argv[1]
 
 name = 'hubert' # update with desired model
 model = S3PRLUpstream(name).cuda()
@@ -22,19 +23,19 @@ with open(f'data.json', 'r') as f:
 
 states = {}
 with torch.no_grad():
-    wavs = [torch.FloatTensor(read(audio['wav_path'])[1]).reshape(-1, 1) for name, audio in data['test'].items() if lang_id[name] == sys.argv[1]]
+    wavs = [torch.FloatTensor(read(audio['wav_path'])[1]).reshape(-1, 1) for name, audio in data['test'].items() if lang_id[name] == lang_id]
     wav_lens = torch.IntTensor([int(len(wav)) for wav in wavs]).cuda()
     wavs = pad_sequence(wavs, True, 500000).cuda()
 
     wavs = wavs.chunk(batches)
     wav_lens = wav_lens.chunk(batches)
 
-    names = np.split(np.array([k for k in data['test'].keys() if lang_id[k] == sys.argv[1]]), [len(wav_len) for wav_len in wav_lens])
+    names = np.split(np.array([k for k in data['test'].keys() if lang_id[k] == lang_id]), [len(wav_len) for wav_len in wav_lens])
 
     for wav_batch, wav_batch_len, names_batch in zip(wavs, wav_lens, names):
         hidden_states, hidden_states_len = model(wav_batch, wav_batch_len).slice(2)
         
         states = {**states, **{name: state.tolist() for name, state in zip(names_batch, hidden_states)}}
 
-with open(f'states/states{sys.argv[1]}.json', 'w') as f:
+with open(f'states/states{lang_id}.json', 'w') as f:
     f.write(json.dumps(states))
